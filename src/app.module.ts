1e7b5353e2a2cfee116681acc9fa909b9bd6a5e3
Zenfection <kietgolx65234@gmail.com>
2023-09-12 14:53:32 +0700

    hash password

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from 'nestjs-prisma';
+import { UsersModule } from './users/users.module';
+import { CoffeesModule } from './coffees/coffees.module';
+import { IamModule } from './iam/iam.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule.forRoot(), UsersModule, CoffeesModule, IamModule],
   controllers: [AppController],
   providers: [AppService],
 })
